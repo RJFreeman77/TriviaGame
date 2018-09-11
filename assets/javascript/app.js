2aa5f0144bc4e1d6ba92ce5360abd76ec2f2ca75
RyanJF <ryanjfreeman77@gmail.com>
2018-09-10 23:45:06 -0500

    minor updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,34 @@
 // https://bootsnipp.com/snippets/PjyVm
 // for when I am moving the progress bar
+var questionAry = [
+    {
+    questionNumber: 1,
+    questionName: "Harrison Ford",
+    correctAnswer: "option2",
+}, {
+    questionNumber: 2,
+    questionName: "Iron Man",
+    correctAnswer: "option1",
+}, {
+    questionNumber: 3,
+    questionName: "Face Off",
+    correctAnswer: "option3",
+}
+]
+var currentQuestion;
+
+
+
+$(document).ready(function() {
+    $("#start-button").click(function() {
+        $("#jumbotron").hide(500);
+        $(".carousel-wrapper").show(500);
+    });
+
+    $("#question1").change(function() {
+        console.log("change function");
+        var selectedVaule = $("input[name='radioquestion']:checked").val();
+        console.log(selectedVaule);
+    });
+
+});
