5da2d3197898d88806951c56f4642dc50846c29f
RyanJF <ryanjfreeman77@gmail.com>
2018-09-15 00:05:30 -0500

    make lots of progress. need to refactor the way I'm moving on from result slide to next question. need to create final results page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,134 +6,143 @@
 // Figure out a DRY way of comparing user answers to correct answers (objects)
 // Styling
 // 
-        // Gets Link for Theme Song
-        var audioElement = document.createElement("audio");
-        audioElement.setAttribute("src", "assets/captainplanet24.mp3");
+// Gets Link for Theme Song
+// var audioElement = document.createElement("audio");
+// audioElement.setAttribute("src", "assets/captainplanet24.mp3");
 
-        // Theme Button
-        $(".theme-button").on("click", function() {
-          audioElement.play();
-        });
-        $(".pause-button").on("click", function() {
-          audioElement.pause();
-        });
+// // Theme Button
+// $(".theme-button").on("click", function() {
+//   audioElement.play();
+// });
+// $(".pause-button").on("click", function() {
+//   audioElement.pause();
+// });
 
 var questionAry = [
     {
         questionNumber: 1,
-        bandName: "Modest Mouse",
+        bandName: "modestmouse",
         audioSrc: "../audio/modestmouse-floaton.mp3",
     }, {
         questionNumber: 2,
-        bandName: "Blink 182",
+        bandName: "blink182",
         audioSrc: "../audio/blink182-imissyou.mp3",
     }, {
         questionNumber: 3,
-        bandName: "My Chemical Romance",
+        bandName: "mychemicalromance",
         audioSrc: "../audio/mychemicalromance-imnotokay.mp3",
     }, {
         questionNumber: 4,
-        bandName: "Taking Back Sunday",
+        bandName: "takingbacksunday",
         audioSrc: "../audio/takingbacksunday-cutewithoutthee.mp3",
     }, {
         questionNumber: 5,
-        bandName: "Reel Big Fish",
+        bandName: "reelbigfish",
         audioSrc: "../audio/reelbigfish-takeonme.mp3",
     }]
 
-var questionNum = 0;
-
+var questionNum = 1;
+var userScore = 0;
 function checkAnswer(userAnswer) {
-    console.log("checking answer())");
-    isQuestion = false;
+    timer.stop();
     $('.carousel').carousel("next");
-    if (userAnswer === questionAry[questionNum].bandName) {
-        console.log("CORRECT");
-        // show correct answer stuff
-        // move onto answer slide
-        // start 10 sec timer
-        $('#carousel-div').on('slid.bs.carousel', function () {
+    if (questionNum <= questionAry.length) {
+        if (userAnswer === questionAry[questionNum-1].bandName) {
+            userScore++;
+            console.log("CORRECT");
+            $(".correct").show();
+            // move onto answer slide
+            // start 10 sec timer
             timer.start();
-            console.log("slid.bs.carousel");
-        })
+        } else if (userAnswer === "out-of-time") {
+            $(".out-of-time").show();
+            // move to answer slide
+            // start 10 sec time
+            timer.start();
+        } else {
+            console.log("WRONG");
+            $(".wrong").show();
+            // move onto answer slide
+            // start 10 sec timer
+            timer.start();
+        }
+        questionNum++;
+        console.log(questionNum);
     } else {
-        console.log("WRONG");
-        // show incorrect answer stuff\
-        // move onto answer slide
-        // start 10 sec timer
-        $('#carousel-div').on('slid.bs.carousel', function () {
-            timer.start();
-            console.log("slid.bs.carousel");
-        })
+        timer.stop();
+        goToResultsPage();
     }
-    questionNum++;
+
 }
 
+function goToResultsPage() {
+    $(".carousel-wrapper").hide(500);
+    $(".jumbotron2").show(500);
+}
 
 
 
 $(document).ready(function () {
     $("#start-button").click(function () {
-        questionNum = 0;
-        $("#jumbotron").hide(500);
+        questionNum = 1;
+        $("#jumbotron1").hide(500);
         $(".carousel-wrapper").show(500);
         timer.start();
-
-
     });
 
-    $("#question1").change(function () {
-        var selectedValue = $("input[name='radioquestion']:checked").val();
+    $(".form-check-input").click(function () {
+        var selectedValue = $(this).attr("value");
         checkAnswer(selectedValue);
+        console.log(this);
+        console.log($(this).attr("value"));
     });
-    
-    if (timer.time <= 0) {
-        timer.stop();
-        checkAnswer();
-    } 
+
 
 });
 
 // timer set up
-var time = 20;
 var intervalId;
+var timerDisplay = $("#timer-bar");
 var isQuestion = true;
-var timerDisplay = $("#timer-display");
-
 var timer = {
-    time: 20,
+    time: 45,
     reset: function () {
-        // timer.time = 20;
+        timer.time = 45;
         timerDisplay.text("");
+        isQuestion = true;
     },
     start: function () {
         if (isQuestion) {
-            console.log("if isQuestion");
-            timer.time = 20;
+            timer.time = 5;
             intervalId = setInterval(timer.count, 1000);
+            isQuestion = false;
         } else if (!isQuestion) {
-            console.log("if isnot isQuestion");
-            timer.time = 10;
+            timer.time = 5;
             intervalId = setInterval(timer.count, 1000);
+            isQuestion = true;
         } else {
-            console.log("something is wrong with the timer");
+            console.log('something is wrong with start function');
         }
     },
     stop: function () {
+        console.log("STOP");
         clearInterval(intervalId);
         clockRunning = false;
     },
     count: function () {
         timer.time--;
         console.log("time", timer.time);
+        timerDisplay.text(timer.time);
         if (timer.time <= 0) {
-            timer.stop();
-            checkAnswer();
+            hideResult();
+            checkAnswer("out-of-time");
         }
-        // timerDisplay.text(timer.time);
     }
 }
 
+function hideResult() {
+    $(".result").hide();
+}
 
 
 // $('#myCarousel').on('slid.bs.carousel', function () {
